bfb0b88f356b4014aee0f87bc110c78e805edfdf
carlosrafael426 <carlosrafael360@gmail.com>
2026-08-23 18:21:34 -0300

    Remove Santa Faustina/Divina Misericórdia split and bottom CTA strip
    Both sections are dropped from the Home page per request. Deleted the
    now-unused SaintFeatureSplit and ContactCtaStrip components along with
    their orphaned images (rosario.jpg, igreja-interior.jpg); the standalone
    /santa-faustina and /divina-misericordia pages are unaffected.

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,8 @@
 import { Hero } from '../components/sections/Hero'
 import { InfoBar } from '../components/sections/InfoBar'
 import { Pillars } from '../components/sections/Pillars'
-import { SaintFeatureSplit } from '../components/sections/SaintFeatureSplit'
 import { CommunityCarousel } from '../components/sections/CommunityCarousel'
 import { EventsNewsPreview } from '../components/sections/EventsNewsPreview'
-import { ContactCtaStrip } from '../components/sections/ContactCtaStrip'
 
 export function Home() {
   return (
@@ -12,10 +10,8 @@ export function Home() {
       <Hero />
       <InfoBar />
       <Pillars />
-      <SaintFeatureSplit />
       <CommunityCarousel />
       <EventsNewsPreview />
-      <ContactCtaStrip />
     </>
   )
 }
